76b971cce6c2f55e19c1cec739ac4b9e7445dc29
Chris Mittell <mittell@hotmail.co.uk>
2024-09-04 17:22:29 +0100

    feat: add base sitemap

---
+++ app/sitemap.tsx
@@ -0,0 +1,49 @@
+import { MetadataRoute } from 'next';
+
+import { posts } from '#site/content';
+import { siteConfig } from '@/config/site';
+import { getAllTags, sortTagsByCount } from '@/lib/utils';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+	const sitemapPost: MetadataRoute.Sitemap = posts.map((post) => {
+		return {
+			url: `${siteConfig.url}/blog/${post.slugAsParams}`,
+			priority: 1.0,
+			changeFrequency: 'daily',
+			lastModified: post.date,
+		};
+	});
+	const tags = getAllTags(posts);
+	const sortedTags = sortTagsByCount(tags);
+
+	const sitemapPostTags: MetadataRoute.Sitemap = sortedTags.map((tag) => {
+		return {
+			url: `${siteConfig.url}/tags/${tag}`,
+			priority: 1.0,
+			changeFrequency: 'daily',
+		};
+	});
+
+	return [
+		{
+			url: `${siteConfig.url}`,
+			priority: 1.0,
+			changeFrequency: 'daily',
+			lastModified: new Date(),
+		},
+		{
+			url: `${siteConfig.url}/blog`,
+			priority: 1.0,
+			changeFrequency: 'daily',
+			lastModified: new Date(),
+		},
+		{
+			url: `${siteConfig.url}/tags`,
+			priority: 1.0,
+			changeFrequency: 'daily',
+			lastModified: new Date(),
+		},
+		...sitemapPost,
+		...sitemapPostTags,
+	];
+}
